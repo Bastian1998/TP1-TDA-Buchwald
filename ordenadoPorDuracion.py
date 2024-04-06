09bea78fdf7a5b0c7addae0370bac30ab4a4b531
Seba <sebastian.arroyo@creativecoefficient.net>
2024-04-06 19:08:25 -0300

    Added resultados_peso and resultados_tiempo pngs

--- ordenadoPorDuracion.py
+++ ordenadoPorDuracion.py
@@ -10,7 +10,7 @@ def merge(lista1, lista2):
     i, j = 0, 0
     result = []
     while(i < len(lista1) and j < len(lista2)):
-        if (lista1[i][0] < lista2[j][0]):
+        if (lista1[i][0] < lista2[j][0]): #Ti, Bi (duración, importancia)
             result.append(lista1[i])
             i += 1
         else:
